0b4ed4ca5a3cd0a037621a7ea6b792eee53dfed3
mosherman <moriahsherman@gmail.com>
2016-06-01 14:22:30 +0100

    Adding the room model to the Django admin.

--- museum/admin.py
+++ museum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Room
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('number', 'name', 'tag_1', 'tag_2', 'tag_3', 'description')
+
+admin.site.register(Room, RoomAdmin)
